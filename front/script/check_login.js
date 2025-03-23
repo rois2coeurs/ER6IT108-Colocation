b8fa5894b07dda4670f1bb3c641099f80d465480
rois2coeurs <rois2coeurs@gmail.com>
2025-03-23 15:24:28 +0100

    forgotten console.log

--- front/script/check_login.js
+++ front/script/check_login.js
@@ -4,9 +4,7 @@ const api = new ApiClient();
 
 async function checkLogin() {
     const res = await api.get("/validate");
-    console.log(res);
     if (!res.ok) {
-        console.log("Token invalid");
         localStorage.removeItem('token');
         window.location.href = 'login.html';
     }
