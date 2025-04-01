2f0ec13e1e388e49dc6d534cd79a27254ebde38d
nicolas <nicolas.thongphao@gmail.com>
2025-04-01 11:21:45 +0200

    refactor shared fund routes by removing unused methods and adding endpoints for retrieving shared fund details and payments

--- back/routes/sharedFundRoutes.ts
+++ back/routes/sharedFundRoutes.ts
@@ -9,16 +9,6 @@ export default {
             AuthHelper.checkAuth(req);
             const houses = await getAllHouseShares();
             return Response.json(houses);
-        },
-        POST: async (req: BunRequest<"/house-share">) => {
-            const userId = AuthHelper.checkAuth(req);
-
-            const {name, address} = await req.json();
-            if (!name || !address) throw new SafeDisplayError("Missing fields", 400);
-
-            const res = await newHouseShare(name, address, userId);
-
-            return Response.json(res[0], {status: 201});
         }
     },
     '/house-share/:id': {
@@ -28,32 +18,6 @@ export default {
             const house = await getHouseShare(Number(id));
             if (!house[0]) throw new SafeDisplayError("house-share not found!", 404);
             return Response.json(house[0]);
-        },
-        DELETE: async (req: BunRequest<"/house-share/:id">) => {
-            const userId = AuthHelper.checkAuth(req);
-            const {id} = req.params;
-
-            const house = await getHouseShare(Number(id));
-            if (!house[0]) throw new SafeDisplayError("house-share not found!", 404);
-            if (house[0].manager_id !== userId) throw new UnauthorizedError();
-            await deleteHouseShare(id);
-
-            return new Response("House-share deleted", {status: 200});
-        },
-        PUT: async (req: BunRequest<"/house-share/:id">) => {
-            const userId = AuthHelper.checkAuth(req);
-            const {id} = req.params;
-
-            const house = await getHouseShare(Number(id));
-            if (!house[0]) throw new SafeDisplayError("house-share not found!", 404);
-            if (house[0].manager_id !== userId) throw new UnauthorizedError();
-
-            const {name, address} = await req.json();
-            if (!name || !address) throw new SafeDisplayError("Missing fields", 400);
-
-            await updateHouseShare(name, address, Number(id));
-
-            return Response.json({message: "House-share updated"}, {status: 200});
         }
     },
     '/house-share/:id/members' : {
@@ -62,34 +26,22 @@ export default {
             const {id} = req.params;
             const members = await getHouseShareMembers(Number(id));
             return Response.json(members);
-        },
-        POST: async (req: BunRequest<"/house-share/:id/members">) => {
-            const userId = AuthHelper.checkAuth(req);
+        }
+    },
+    '/house-share/:id/shared-fund' : {
+        GET: async (req: BunRequest<"/house-share/:id/shared-fund">) => {
+            AuthHelper.checkAuth(req);
             const {id} = req.params;
-            const house = await getHouseShare(Number(id));
-
-            if (!house[0]) throw new SafeDisplayError("house-share not found!", 404);
-            if (house[0].manager_id !== userId) throw new UnauthorizedError("Only the manager can add members");
-            const {email} = await req.json();
-
-            if (!email) throw new SafeDisplayError("Missing email field", 400);
-            const user = await getUserByEmail(email);
-            if (!user[0]) throw new SafeDisplayError("User not found", 404);
-            await addHouseShareMember(user[0].id, Number(id));
-
-            return Response.json({message: "User added to house-share"}, {status: 201});
-        },
-        PUT: async (req: BunRequest<"/house-share/:id/members">) => {
-            const userId = AuthHelper.checkAuth(req);
+            const members = await getHouseShareSharedFund(Number(id));
+            return Response.json(members);
+        }
+    },
+    '/house-share/:id/shared-fund/payments' : {
+        GET: async (req: BunRequest<"/house-share/:id/shared-fund/payments">) => {
+            AuthHelper.checkAuth(req);
             const {id} = req.params;
-            const house = await getHouseShare(Number(id));
-
-            if (!house[0]) throw new SafeDisplayError("house-share not found!", 404);
-            const membership = await checkMembership(userId, Number(id));
-            if (!membership[0]) throw new SafeDisplayError("You are not a member of this house-share", 400);
-            await updateHouseShareMember(userId, Number(id));
-
-            return Response.json({message: "Successfully left the house-share"}, {status: 200});
+            const members = await getHouseShareSharedFundPayments(Number(id));
+            return Response.json(members);
         }
     }
 }
@@ -105,25 +57,6 @@ async function getHouseShare(id: number) {
                WHERE id = ${id};`;
 }
 
-async function newHouseShare(name: string, address: string, userId: number) {
-    return sql`INSERT INTO house_share (name, address, manager_id)
-               VALUES (${name}, ${address}, ${userId})
-               RETURNING id;`;
-}
-
-async function deleteHouseShare(id: string) {
-    await sql`DELETE
-              FROM house_share
-              WHERE id = ${id};`;
-}
-
-async function updateHouseShare(name: string, address: string, id: number) {
-    await sql`UPDATE house_share
-              SET name    = ${name},
-                  address = ${address}
-              WHERE id = ${id};`;
-}
-
 async function getHouseShareMembers(id: number) {
     return sql`SELECT users.id, users.name, users.firstname, users.mail, users.phone_number, 
                       stays.entry_date, stays.exit_date
@@ -132,26 +65,19 @@ async function getHouseShareMembers(id: number) {
             WHERE stays.house_share_id = ${id};`;
 }
 
-async function getUserByEmail(email: string) {
-    return sql`SELECT *
-               FROM users 
-               WHERE mail = ${email};`;
+async function getHouseShareSharedFund(id: number) {
+    return sql`SELECT name
+               FROM house_share
+               WHERE id = ${id};`;
 }
 
-async function checkMembership(userId: number, houseId: number) {
-    return sql`SELECT *
-               FROM stays
-               WHERE user_id = ${userId} AND house_share_id = ${houseId} AND exit_date IS NULL
-               LIMIT 1;`;
-}
-
-async function addHouseShareMember(userId: number, id: number) {
-    await sql`INSERT INTO stays (entry_date, user_id, house_share_id)
-                VALUES (CURRENT_DATE, ${userId}, ${id});`;
-}
-
-async function updateHouseShareMember(userId: number, id: number) {
-    await sql`UPDATE stays 
-            SET exit_date = CURRENT_DATE
-            WHERE house_share_id = ${id} AND user_id = ${userId} AND exit_date IS NULL;`;
+async function getHouseShareSharedFundPayments(id: number) {
+    return sql`
+        SELECT users.name, users.firstname, contributions.date, contributions.amount
+        FROM house_share
+        INNER JOIN shared_fund ON shared_fund.house_share_id = house_share.id
+        INNER JOIN contributions ON contributions.shared_fund_id = shared_fund.id
+        INNER JOIN users ON contributions.user_id = users.id
+        WHERE house_share.id = ${id}
+        ORDER BY contributions.date DESC;`;
 }
