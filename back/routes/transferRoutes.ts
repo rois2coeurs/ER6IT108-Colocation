d1fb2196b4f848b1a500eda66ed658f11a018ed7
rois2coeurs <rois2coeurs@gmail.com>
2025-04-06 01:03:13 +0200

    working transfer

---
+++ back/routes/transferRoutes.ts
@@ -0,0 +1,52 @@
+import {type BunRequest, sql} from "bun";
+import {AuthHelper} from "../helpers/authHelper.ts";
+import {UnauthorizedError} from "../errors/UnauthorizedError.ts";
+import {SafeDisplayError} from "../errors/SafeDisplayError.ts";
+
+export default {
+    '/transfer/:userId': {
+        GET: async (req: BunRequest<"/transfer/:userId">) => {
+            const currentUserId = AuthHelper.checkAuth(req);
+            const {userId} = req.params;
+            if (currentUserId !== Number(userId)) throw new UnauthorizedError("You are not authorized to view this user's transfers");
+            const transfers = await getUserTransfers(Number(userId));
+            return Response.json(transfers);
+        },
+        POST: async (req: BunRequest<"/transfer/:userId">) => {
+            const currentUserId = AuthHelper.checkAuth(req);
+            const {userId} = req.params;
+            if (currentUserId !== Number(userId)) throw new UnauthorizedError("You are not authorized to make transfers for this user");
+            const {amount, recipientEmail} = await req.json();
+            if (!amount || !recipientEmail) throw new SafeDisplayError("Missing fields", 400);
+            if (amount <= 0) throw new SafeDisplayError("Amount must be greater than 0", 400);
+            const recipient = await getUserByEmail(recipientEmail);
+            if (!recipient[0]) throw new SafeDisplayError("Recipient not found", 404);
+            if (currentUserId === recipient[0].id) throw new SafeDisplayError("You cannot transfer to yourself", 400);
+            await createTransfer(currentUserId, recipient[0].id, amount);
+            return Response.json({message: "Transfer created"}, {status: 201});
+        }
+    }
+}
+
+function getUserTransfers(userId: number) {
+    return sql`SELECT transfers.id, transfers.amount, transfers.date, users.firstname, users.name, True as is_sender
+               FROM transfers
+               INNER JOIN users ON transfers.receiver_id = users.id
+               WHERE sender_id = ${userId}
+               UNION 
+               SELECT transfers.id, transfers.amount, transfers.date, users.firstname, users.name, False as is_sender
+               FROM transfers
+               INNER JOIN users ON transfers.sender_id = users.id
+               WHERE receiver_id = ${userId}`;
+}
+
+function getUserByEmail(email: string) {
+    return sql`SELECT id
+               FROM users
+               WHERE mail = ${email}`;
+}
+
+function createTransfer(currentUserId: number, recipientId: number, amount: number, date: Date = new Date()) {
+    return sql`INSERT INTO transfers (sender_id, receiver_id, amount, date)
+               VALUES (${currentUserId}, ${recipientId}, ${amount}, ${date.toISOString()})`;
+}
