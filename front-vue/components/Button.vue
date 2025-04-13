8cb33eb597b5e46e3b1a052d100563b4368fb057
rois2coeurs <rois2coeurs@gmail.com>
2025-04-13 14:38:02 +0200

    user page

---
+++ front-vue/components/Button.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  buttonText: {
+    type: String,
+    default: 'Afficher Tout'
+  },
+  onButtonClick: Function
+});
+</script>
+
+<template>
+  <button class="toggle" v-on:click="onButtonClick">{{ buttonText }}</button>
+</template>
+
+<style scoped>
+.toggle {
+  background-color: #EB5160FF;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  padding: 10px 15px;
+  font-size: 16px;
+  cursor: pointer;
+  width: 100%;
+  transition: background-color 0.3s;
+}
+
+.toggle:hover {
+  background-color: #d83848;
+}
+</style>
