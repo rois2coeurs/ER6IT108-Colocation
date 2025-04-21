a135769bf7bb7dd10d31c7697ff37de8c98e7c0a
rois2coeurs <rois2coeurs@gmail.com>
2025-04-21 02:32:18 +0200

    Checkbox FormInput

--- front-vue/components/Form/Input.vue
+++ front-vue/components/Form/Input.vue
@@ -19,7 +19,10 @@ const value = defineModel('value');
 <template>
   <div class="form-input-group">
     <label :for="name">{{ label }}</label>
-    <input :type="inputType" :name="name" type="{{inputType}}" v-model="value" />
+    <div v-if="inputType === 'checkbox'" class="checkbox-wrapper">
+      <input type="checkbox" :name="name" v-model="value" :id="name"/><label :for="name"></label>
+    </div>
+    <input v-else :type="inputType" :name="name" v-model="value"/>
   </div>
 </template>
 
@@ -35,5 +38,48 @@ const value = defineModel('value');
   border: 1px solid #A19C9C;
   background-color: #f8f8f8;
   padding: 10px;
+}
+
+.checkbox-wrapper input[type=checkbox] {
+  height: 0;
+  width: 0;
+  display: none;
+}
+
+.checkbox-wrapper label {
+  --size: 35px;
+
+  cursor: pointer;
+  width: var(--size);
+  height: calc(var(--size) / 2);
+  background: grey;
+  display: block;
+  border-radius: 100px;
+  position: relative;
+}
+
+.checkbox-wrapper label:after {
+  content: '';
+  position: absolute;
+  top: 6%;
+  left: 2.5%;
+  width: calc(50% - 5%);
+  height: calc(100% - 11%);
+  background: #fff;
+  border-radius: 90px;
+  transition: 0.3s;
+}
+
+.checkbox-wrapper input:checked + label {
+  background: #EB5160FF;
+}
+
+.checkbox-wrapper input:checked + label:after {
+  left: calc(100% - 2.5%);
+  transform: translateX(-100%);
+}
+
+.checkbox-wrapper label:active:after {
+  width: 55%;
 }
 </style>
