9b3cfe10597c605a33db142d71e94c12f399b2ee
rois2coeurs <rois2coeurs@gmail.com>
2025-04-21 02:33:33 +0200

    purchase frontend

---
+++ front-vue/types/purchase.d.ts
@@ -0,0 +1,9 @@
+declare interface Purchase {
+    id: number
+    title: string
+    amount: number
+    date: string
+    shared_fund_set: boolean
+    house_share_name: string
+    house_share_id: number
+}
