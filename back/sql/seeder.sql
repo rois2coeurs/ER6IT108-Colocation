8f087f36549c6e74e6eec1d773a14882676b50fa
rois2coeurs <rois2coeurs@gmail.com>
2025-04-21 02:30:18 +0200

    mroe random data in seeder

--- back/sql/seeder.sql
+++ back/sql/seeder.sql
@@ -55,12 +55,43 @@ VALUES (1, 1, '2025-01-02', 10.00);
 
 -- A little Payment (Versement) from the first administrator too
 INSERT INTO transfers (sender_id, receiver_id, date, amount)
-VALUES ( 1, 2, '2025-01-03', 5.00);
+VALUES (1, 2, '2025-01-03', 5.00);
 
 -- A little Achat from the first administrator too
 INSERT INTO purchases (title, amount, date, shared_fund_id, user_id)
-VALUES ('Crousty', 15.00, '2025-01-04', 1, 1);
-
+VALUES ('Crousty', 15.00, '2025-01-04', 1, 1),
+       ('Tacos', 8.50, '2025-01-04', 2, 2),
+       ('Salade', 6.75, '2025-01-05', 2, 2),
+       ('Pizza', 18.90, '2025-01-06', 2, 2),
+       ('Sushi', 25.00, '2025-01-07', 2, 2),
+       ('Burger', 12.00, '2025-01-08', 2, 2),
+       ('Pâtes', 14.30, '2025-01-09', 2, 2),
+       ('Sandwich', 5.50, '2025-01-10', null, 2),
+       ('Glace', 7.20, '2025-01-11', null, 2),
+       ('Boisson', 3.00, '2025-01-12', null, 2),
+       ('Chips', 2.50, '2025-01-13', null, 2),
+       ('Chocolat', 4.00, '2025-01-14', null, 2),
+       ('Bonbons', 3.75, '2025-01-15', null, 2),
+       ('Café', 2.00, '2025-01-16', null, 2),
+       ('Thé', 1.80, '2025-01-17', null, 2),
+       ('Croissant', 1.50, '2025-01-18', null, 2),
+       ('Pain', 1.20, '2025-01-19', null, 2),
+       ('Fromage', 6.00, '2025-01-20', null, 2),
+       ('Jus d''orange', 3.50, '2025-01-21', null, 2),
+       ('Lait', 1.00, '2025-01-22', null, 2),
+       ('Céréales', 4.50, '2025-01-23', null, 2),
+       ('Yaourt', 2.30, '2025-01-24', null, 2),
+       ('Poulet', 10.00, '2025-01-25', null, 2),
+       ('Poisson', 12.50, '2025-01-26', null, 2),
+       ('Riz', 3.00, '2025-01-27', null, 2),
+       ('Pâtes fraîches', 5.00, '2025-01-28', null, 2),
+       ('Vin', 15.00, '2025-01-29', null, 2),
+       ('Bière', 2.50, '2025-01-30', null, 2),
+       ('Eau', 1.00, '2025-01-31', null, 2),
+       ('Fruits', 7.00, '2025-02-01', null, 2),
+       ('Légumes', 8.00, '2025-02-02', null, 2),
+       ('Pizza surgelée', 6.50, '2025-02-03', null, 2),
+       ('Gâteau', 10.00, '2025-02-04', null, 2);
 -- Create some sojourns for regular members
 INSERT INTO stays (entry_date, exit_date, user_id, house_share_id)
 VALUES ('2025-02-01', NULL, (SELECT id from users where mail = 'adrien.baril@gmail.com'), 1),
